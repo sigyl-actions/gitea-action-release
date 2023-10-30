8b33a4917362e77c2f298175edd5b84f5bdb1e31
gilesb <giles.bradshaw@sigyl.com>
2023-10-30 15:22:28 +0000

    better default

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ async function run() {
 
     const api = new giteaApi(
       core.getInput('serverUrl')
-        || (github.context.runId && github.context.serverUrl)
-        || 'https://gitea.com',
+        || (github.context.runId && github.context.serverUrl),
       {
         token: core.getInput('token'),
         customFetch: fetch,
@@ -19,7 +18,7 @@ async function run() {
     
     const [owner, repo] = (
       core.getInput('repository')
-        || github.context.repository
+        || github.context.payload.repository.full_name
     ).split("/");
     await api.repos.repoCreateRelease(
       owner,
